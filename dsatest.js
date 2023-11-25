b0521c49f55e2dd2ad7ad00832264d2bda814d0c
sneharajpurkar <devshdighe4d@gmail.com>
2023-11-25 17:07:33 +0530

    update

---
+++ dsatest.js
@@ -0,0 +1,10 @@
+//Q1
+ var num =[2,7,11,15];
+ target=9;
+ for (let i=0; i<=num.length; i++){
+ for(let j=0;j<=num.length; j++){
+     if(num[i]+num[j]==target){
+        return [i,j]
+    }
+ }
+ }
